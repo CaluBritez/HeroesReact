56768daa7dca6d8aa9ace1f14d00c98750232d51
Calu <lucas_britez@hotmail.com>
2024-07-19 03:45:23 -0300

    login y logout terminados

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,9 +1,16 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext.jsx';
 
 export const Login = () => {
 
+  const {login} = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogin = () => {
+    
+    
+    login("Calubich");
+    
     navigate('/', {
       replace: true
     })
