b8534a04c2a5b200568b13c91f609665badcc09e
Calu <lucas_britez@hotmail.com>
2024-07-19 04:29:37 -0300

    rutas privadas y publicas agregadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,33 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
-import { HeroesRoutes } from "../heroes"
-import { Login } from "../auth"
+import { HeroesRoutes } from "../heroes";
+import { Login } from "../auth";
+
+import { PrivateRoutes } from "./PrivateRoutes.jsx";
+import { PublicRoutes } from "./PublicRoutes.jsx";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
 
+      <Route path="login" element={
+        <PublicRoutes>
+          <Login />
+        </PublicRoutes>
+        }>
+
+      </Route>
+
         <Route path="login" element={<Login />}></Route>
 
-        <Route path="/*" element={<HeroesRoutes />}></Route>
+        <Route path="/*" element={
+            <PrivateRoutes>
+              <HeroesRoutes />
+            </PrivateRoutes>
+  
+        }>
+        </Route>
 
       </Routes>
     
